6718fa0058b60fb1414f4bc9806335b1e649599a
sorceriv <sorceriv@gmail.com>
2024-04-09 15:12:55 +0800

    Working on navbar mobile, added resume download

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,3 +1,5 @@
+import resume from "../../assets/resume.pdf";
+
 import Hamburger from "./Hamburger";
 import ModeToggle from "./ModeToggle";
 import { useAnimate } from "framer-motion";
@@ -39,7 +41,7 @@ function NavBar({hamburgerActive}: Props) {
               <li><a href="#skills">Skills</a></li>
               <li><a href="#projects">Projects</a></li>
               <li><a href="#contact">Contact</a></li>
-              <li><a href="#Resume">Resume <svg style={{display: "inline",}} viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.87868 34.8198C-0.292893 35.9914 -0.292893 37.8909 0.87868 39.0625C2.05025 40.2341 3.94975 40.2341 5.12132 39.0625L0.87868 34.8198ZM39.9411 3.00004C39.9411 1.34318 38.598 3.47445e-05 36.9411 3.54043e-05L9.94112 3.48564e-05C8.28427 3.49263e-05 6.94112 1.34318 6.94112 3.00004C6.94112 4.65689 8.28427 6.00004 9.94112 6.00004L33.9411 6.00004L33.9411 30C33.9411 31.6569 35.2843 33 36.9411 33C38.598 33 39.9411 31.6569 39.9411 30L39.9411 3.00004ZM5.12132 39.0625L39.0624 5.12135L34.8198 0.878714L0.87868 34.8198L5.12132 39.0625Z" fill="currentcolor"></path></svg></a></li>
+              <li><a href={ resume } target="_blank">Resume <svg style={{display: "inline",}} viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.87868 34.8198C-0.292893 35.9914 -0.292893 37.8909 0.87868 39.0625C2.05025 40.2341 3.94975 40.2341 5.12132 39.0625L0.87868 34.8198ZM39.9411 3.00004C39.9411 1.34318 38.598 3.47445e-05 36.9411 3.54043e-05L9.94112 3.48564e-05C8.28427 3.49263e-05 6.94112 1.34318 6.94112 3.00004C6.94112 4.65689 8.28427 6.00004 9.94112 6.00004L33.9411 6.00004L33.9411 30C33.9411 31.6569 35.2843 33 36.9411 33C38.598 33 39.9411 31.6569 39.9411 30L39.9411 3.00004ZM5.12132 39.0625L39.0624 5.12135L34.8198 0.878714L0.87868 34.8198L5.12132 39.0625Z" fill="currentcolor"></path></svg></a></li>
             </ul>
           </nav>
       </>
